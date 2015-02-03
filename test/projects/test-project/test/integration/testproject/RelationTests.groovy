b3e5993e52a57debf3f601d5fd201899a391c4f5
Craig Weston <craig@craigweston.ca>
2015-02-03 15:57:35 -0500

    Cleaned up integration test names

--- test/projects/test-project/test/integration/testproject/RelationTests.groovy
+++ test/projects/test-project/test/integration/testproject/RelationTests.groovy
@@ -4,27 +4,22 @@ import spock.lang.Specification
 
 class RelationTests extends Specification {
 
-    public void testDefaultDataSource() {
+    public void "test default datasource"() {
         setup:
-
             Foo foo = new Foo(foo: "foo1")
             foo.save()
 
             Bar bar = new Bar(bar: "bar1")
             bar.save()
-
         when:
-
             foo.setBar(bar)
-
         then:
 
             foo.getBar() == bar
     }
 
-    public void testIsDatasourceAware() {
+    public void "test is datasource aware"() {
         setup:
-
             Bar bar1 = new Bar(bar: "bar1")
             bar1.save()
 
@@ -33,13 +28,9 @@ class RelationTests extends Specification {
 
             FooLookup fooLookup = new FooLookup(foo: "fooLookup")
             fooLookup.save()
-
         when:
-
             fooLookup.setBar(bar2)
-
         then:
-
             bar2.id
             Bar.lookup.get(bar2.id) == bar2
             Bar.get(bar2.id) != bar2
